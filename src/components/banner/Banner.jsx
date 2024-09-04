72e22c8090d724faf2b914287a50798cb4a9dd70
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-09-04 14:01:02 +0600

    banner complete

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,22 +2,15 @@
 
 function Banner() {
   return (
-    <div className="relative h-64 bg-cover bg-center" style={{ backgroundImage: "url('https://i.postimg.cc/HLz4xy1K/banner-1.jpg')" }}>
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-black opacity-80"></div>
-      
-      {/* Content */}
-      <div className="relative h-full flex items-center justify-between px-8">
-        {/* Left Text */}
-        <div className="text-white text-3xl font-bold">
-          Twitter
-        </div>
+    <div className="relative h-[300px] bg-cover bg-center" style={{ backgroundImage: "url('https://i.postimg.cc/HLz4xy1K/banner-1.jpg')" }}>
 
-        {/* Right Text */}
-        <div className="text-white text-3xl font-bold">
-          Twitter
-        </div>
+      <div className="absolute inset-0 bg-black opacity-80"></div>
+
+      <div className="relative h-full flex items-center md:justify-between justify-center md:gap-0 gap-5 md:flex-row flex-col md:px-28">
+          <h1 className="md:text-5xl text-4xl font-bold text-[#FFFFFF]">Study In USA</h1> 
+          <h5 className="text-base font-normal text-[#FFFFFF]">Home /Study Distention/USA</h5>
       </div>
+
     </div>
   );
 }
